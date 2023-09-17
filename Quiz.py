fd7807688f25040b790253c9598cde66ec8d707e
lol158 <muri5988@gmail.com>
2023-09-17 19:15:45 +0200

    updated quiz with "frage" function

--- Quiz.py
+++ Quiz.py
@@ -10,6 +10,9 @@ Datum:15.09.23
 print("Hallo und viel Spass bei meinem Quiz! \n\n ")
 
 #---------------------------------------------------------------------------------------#
+#lebenspunkte und punkte
+lp = 0
+points = 0
 
 #Schwierigkeits Auswahl
 print("Waehlen die Schwierigkeit aus auf der sie spielen wollen! \n\n\t" 
@@ -19,16 +22,20 @@ print("Waehlen die Schwierigkeit aus auf der sie spielen wollen! \n\n\t"
 
 while True:
     input_string = input("Ihre Wahl?: ")
+    print('--------------------------------------------------------------')
     difficulty = input_string.lower()
 
     if difficulty == "l":
-        print("Sie Haben den leichten Schwierigkeitsgrad gewaehlt. Sie haben 3 Leben, viel glueck!")
+        lp += 3
+        print(f"\n\n\nSie Haben den leichten Schwierigkeitsgrad gewaehlt. Sie haben {lp} Leben, viel glueck! \n\n\n")
         break
     elif difficulty == "m":
-        print("Sie Haben den mittleren Schwierigkeitsgrad gewaehlt. Sie haben 2 Leben, viel glueck!")
+        lp += 2
+        print(f"\n\n\nSie Haben den mittleren Schwierigkeitsgrad gewaehlt. Sie haben {lp} Leben, viel glueck! \n\n\n")
         break
     elif difficulty == "s":
-        print("Sie Haben den schweren Schwierigkeitsgrad gewaehlt. Sie haben 1 Leben, viel glueck!")
+        lp += 1
+        print(f"\n\n\nSie Haben den schweren Schwierigkeitsgrad gewaehlt. Sie haben {lp} Leben, viel glueck! \n\n\n")
         break
     else:
         print("Ungueltige Eingabe!")
@@ -37,22 +44,80 @@ while True:
 
 
 #Fragen
+def frage(frage, antw1, antw2, antw3, loesung):
 
-points = 0
+    global points
+    global lp
+
+    print(frage + "\n") 
+    print(f"Antwort 1: {antw1}")
+    print(f"Antwort 2: {antw2}")
+    print(f"Antwort 3: {antw3}")
+    print('--------------------------------------------------------------')
+
+    antwort = int(input("Welche Antwort ist die Richtige?: "))
+    print('--------------------------------------------------------------')
+    if antwort == loesung:
+        print("\nDas ist Richtig!\n")
+        points += 1
+        print(f"\n\nDeine Punktzahl ist nun {points}\n")
+        print('--------------------------------------------------------------')
+    elif antwort != loesung:
+        print("Falsche Antwort!")
+        lp -= 1
+        print(f"\n\nDeine Punktzahl ist nun {points}\n")
+        print('--------------------------------------------------------------')
+        print(f"Sie haben nun {lp} Leben")
+
+
 
 while True:
-    if ph == -1:
+    if lp == 0:
+        print("\n\nSchade viel Glueck beim naechsten mal!")
         break
 
-    frage1 = 3
-    print("Frage 1: Wieviel Bit sind in einem Byte? \n\t"
-          "Antwort 1: 4\n\t"
-          "Antwort 2: 16\n\t"
-          "Antwort 3: 8\n\n")
+    frage("Frage 1: Wieviel Bit sind in einem Byte?", "4", "16", "8", 3)
 
-    antwort = int(input("Welche Antwort ist die richtige? : "))
+    if lp == 0:
+        print("\n\nSchade viel Glueck beim naechsten mal!")
+        break
 
-    if antwort == frage1:
-        print("Das ist Richtig!")
-        points += 1
-        print(f"Deine Punktzahl ist nun {points}")
+    frage('Was bedeutet der Datentyp \"int\" auf Deutsch', 'Integer', 'Ganzzahl', 'Vollstaendige Zahl', 2)
+
+    if lp == 0:
+        print("\n\nSchade viel Glueck beim naechsten mal!")
+        break
+
+    frage("Welches Schlüsselwort wird in Python verwendet, um eine Schleife zu beenden?", "end", "break", "stop", 2)
+
+    if lp == 0:
+        print("\n\nSchade viel Glueck beim naechsten mal!")
+        break
+
+    frage("Welche Art von Variable kann nur zwei Werte haben, True oder False?", "Integer", "String", "Boolean", 3)
+
+    if lp == 0:
+        print("\n\nSchade viel Glueck beim naechsten mal!")
+        break
+
+    frage("Welches Zeichen wird in den meisten Programmiersprachen verwendet, um einen Kommentar zu markieren?", "#", "//", "*", 1)
+
+    if lp == 0:
+        print("\n\nSchade viel Glueck beim naechsten mal!")
+        break
+
+    frage("Welches Python-Modul ermöglicht den Zugriff auf mathematische Funktionen wie Sinus und Quadratwurzel?", "os", "math", "sys", 2)
+
+    if lp == 0:
+        print("\n\nSchade viel Glueck beim naechsten mal!")
+        break
+
+    frage('Wie heisst die 2te Frau von Henry the 8th', 'Anne Boleyn', 'Catherine Howered', 'Catherine Parr', 1)
+
+    if lp == 0:
+        print("\n\nSchade viel Glueck beim naechsten mal!")
+        break
+
+    break
+
+print(f'\n\nDu hast {points} Punkte erreicht!\nGlueckwunsch!')
