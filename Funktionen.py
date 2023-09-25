02b2c1c20f9acdb6bf915dee24e3a74af0f615ba
lol158 <muri5988@gmail.com>
2023-09-25 20:19:20 +0200

    Funktionen Datei erweitert

--- Funktionen.py
+++ Funktionen.py
@@ -67,6 +67,30 @@ Das macht man mit dem Schlüsselwort : import
 import tkinter
 import turtle
 
+pen = turtle.Turtle()
+turtle.speed(0)
+
+def curve():
+  for i in range(200):
+    pen.right(1)
+    pen.forward(1)
+
+def heart():
+  pen.fillcolor('red')
+  pen.begin_fill()
+  pen.left(140)
+  pen.forward(113)
+  curve()
+  pen.left(120)
+  curve()
+  pen.forward(112)
+  pen.end_fill()
+
+heart()
+
+turtle.done()
+pen.ht()
+
 turtle.bgcolor('black')
 t = turtle.Turtle()
 colors = ['red', 'dark red']
