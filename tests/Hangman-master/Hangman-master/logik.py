6830ccc1ddec09ba05d39e54d42ae0f0fca6938d
WarumSindNamenSoSchwer <muri5988@gmail.com>
2023-12-05 09:33:27 +0100

    namens und pfad änderung

--- tests/Hangman-master/Hangman-master/logik.py
+++ tests/Hangman-master/Hangman-master/logik.py
@@ -3,94 +3,92 @@ import random
 import hangmandraw as hD
 import self_destruct as sD
 
-# Lese Wörter aus einer Datei und entferne Leerzeichen
 with open("Hangman/words.txt") as f:
     words = f.readlines()
     words = [x.strip() for x in words]
 
-# Initialisiere verschiedene Listen und Sets für das Spiel
-verschlüsselt = []        # Liste für die verschlüsselte Version des zu ratenden Wortes
-list_wort = []            # Liste für das zu ratende Wort (für Entwicklungs- und Debugging-Zwecke)
-geratene_Buchstaben = set()   # Set für die bereits geratenen Buchstaben
+verschlüsselt = []
+list_wort = []   #dev
+geratene_Buchstaben = set()
 
 istAn = True
 while istAn:
-    # Mische die Wörterliste und wähle ein zufälliges Wort aus
     random.shuffle(words)
     zu_ratendes_wort = words[0]
     
-    # Initialisiere die verschlüsselte Version des Wortes
     for index in zu_ratendes_wort:
         verschlüsselt.append('_')
-        list_wort.append(index)  # Entwicklungs- und Debugging-Zwecke
+        list_wort.append(index) #dev
 
-    print(zu_ratendes_wort)   # Debug-Ausgabe
-    print(list_wort)          # Debug-Ausgabe
-    print(verschlüsselt)      # Debug-Ausgabe
     
-    versuche = 11   # Anzahl der Versuche
-    leben = 11      # Anzahl der Leben
-
+    print(zu_ratendes_wort) #Dev
+    print(list_wort) #Dev
+    print(verschlüsselt) #Dev
+    
+    versuche = 11
+    leben = 11
+    
     while versuche > 0:
-        # Überprüfe, ob das Wort bereits komplett geraten wurde
+        
         for i in range(len(verschlüsselt)):
             if "_" not in verschlüsselt:
                 replay = t.textinput("You won somehow", "Wanna Play again? Yes/No")
                 if replay.lower() == "yes" or replay.lower() == "y":
                     versuche = 11
                     leben = 11
+                    
                     verschlüsselt = []
-                    list_wort = []   # Entwicklungs- und Debugging-Zwecke
+                    list_wort = []   #dev
                     geratene_Buchstaben = set()
-                    print(versuche)  # Debug-Ausgabe
+
+                    print(versuche) #Dev
                     break
                 else:
                     sD.self_destruct()
 
-        # Benutzereingabe für einen Buchstaben oder das gesamte Wort
-        eingabe = t.textinput(verschlüsselt, "Enter a letter or the word, be warned if the whole word is wrong you have lost peasant!\n" +
-                              f"You have {versuche} tries")
+        eingabe = t.textinput(verschlüsselt, "Enter a letter or the word, be warned if the whole word is wrong you have lost peasant!\n"+
+        f"You have {versuche} tries")
         eingabe = eingabe.lower()
 
-        # Überprüfe, ob der Buchstabe bereits geraten wurde
         if eingabe in geratene_Buchstaben:
             print("You already guessed that letter. Try again.")
             continue
 
         geratene_Buchstaben.add(eingabe)
         
-        # Überprüfe erneut, ob das Wort bereits komplett geraten wurde
         for i in range(len(verschlüsselt)):
             if "_" not in verschlüsselt:
                 replay = t.textinput("You won somehow", "Wanna Play again? Yes/No")
                 if replay.lower() == "yes" or replay.lower() == "y":
                     versuche = 11
                     leben = 11
+
                     verschlüsselt = []
-                    list_wort = []   # Entwicklungs- und Debugging-Zwecke
+                    list_wort = []   #dev
                     geratene_Buchstaben = set()
-                    print(versuche)  # Debug-Ausgabe
+
+                    print(versuche) #Dev
                     break
                 else:
                     sD.self_destruct()
 
-        # Überprüfe, ob die Eingabe das gesamte Wort ist und ob es korrekt ist
         if eingabe == zu_ratendes_wort and eingabe.isalpha() and len(eingabe) == len(zu_ratendes_wort):
             print("1")
-            # Einzubauendes Easteregg
+            #einzubauendes easteregg
             hD.easteregg()
             replay = t.textinput("You won somehow", "Wanna Play again? Yes/No")
             if replay.lower() == "yes" or replay.lower() == "y":
                 versuche = 11
                 leben = 11
+
                 verschlüsselt = []
-                list_wort = []   # Entwicklungs- und Debugging-Zwecke
+                list_wort = []   #dev
                 geratene_Buchstaben = set()
-                print(versuche)  # Debug-Ausgabe
+
+                print(versuche) #Dev
                 break
             else:
                 sD.self_destruct()
-        # Überprüfe, ob die Eingabe das gesamte Wort ist und ob es falsch ist
         elif eingabe != zu_ratendes_wort and eingabe.isalpha() and len(eingabe) == len(zu_ratendes_wort):
             versuche = 0
             for i in range(leben):
@@ -99,36 +97,32 @@ while istAn:
             if replay.lower() == "yes" or replay.lower() == "y":
                 versuche = 11
                 leben = 11
-                print(versuche)  # Debug-Ausgabe
+                print(versuche) #Dev
                 break
             else:
                 sD.self_destruct()
 
-        # Überprüfe, ob die Eingabe die richtige Länge hat und ein Buchstabe ist
         if len(eingabe) != len(zu_ratendes_wort) and eingabe.isalpha():
             for i in range(len(zu_ratendes_wort)):
-                # Überprüfe, ob der Buchstabe in der Eingabe vorhanden ist und ob er noch nicht geraten wurde
                 if zu_ratendes_wort[i] in eingabe and verschlüsselt[i] == "_":
                     verschlüsselt[i] = zu_ratendes_wort[i]
-                    print("1")  # Debug-Ausgabe
-                    print(versuche)  # Debug-Ausgabe
+                    print("1")  # Dev
+                    print(versuche)  # Dev
             versuche -= 1
-            # Überprüfe, ob die Eingabe nicht im zu ratenden Wort ist und ob es sich um einen einzelnen Buchstaben handelt
             if eingabe not in zu_ratendes_wort and eingabe.isalpha() and eingabe != " " and len(eingabe) > 1:
                 for i in range(len(eingabe)):
                     leben -= 1
                     hD.Hangmandraw(leben)
                 versuche -= 1
         
-        # Überprüfe, ob der Buchstabe in der Liste des zu ratenden Wortes vorhanden ist
         for i in range(len(list_wort)):
             if eingabe == list_wort[i] and eingabe.isalpha() and verschlüsselt[i] == "_" and eingabe != " ":
                 verschlüsselt[i] = eingabe
-                print("1")  # Debug-Ausgabe
-                print(versuche)  # Debug-Ausgabe
+                print("1")  # Dev
+                print(versuche)  # Dev
 
-        # Überprüfe, ob der Buchstabe nicht im zu ratenden Wort ist und ob
         if eingabe not in list_wort and eingabe.isalpha() and eingabe != " " and len(eingabe) == 1:
             leben -= 1
             versuche -= 1
             hD.Hangmandraw(leben)
+
