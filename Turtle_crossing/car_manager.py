131451f28cbbef1fa417d94926dc1c32fe410b0c
Murat-Can <109090954+WarumSindNamenSoSchwer@users.noreply.github.com>
2024-05-17 09:37:06 +0200

    Add files via upload

--- Turtle_crossing/car_manager.py
+++ Turtle_crossing/car_manager.py
@@ -1,10 +1,21 @@
 # In dieser Datei wird der Auto-Manager programmiert, der Autos erstellt
 # und bewegt.
+import player
+from random import randint
 
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 2
 MOVE_INCREMENT = 2
 
 class CarManager:
-    pass
-    
+    def __init__(self):
+        self.speed = MOVE_INCREMENT
+        self.car_list = []
+        for i in range(10):
+            car = player.Player(color = COLORS[randint(0,5)])
+            self.car_list.append(car)
+        
+        
+    def move_cars(self):
+        for car in self.car_list:
+            car.forward(MOVE_INCREMENT)
