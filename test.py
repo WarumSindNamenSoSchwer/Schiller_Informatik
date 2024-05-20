de135b4d87a9ed68e43ea55874a0c97cd5b17b8a
WarumSindNamenSoSchwer <muri5988@gmail.com>
2024-05-20 20:41:40 +0200

    deleted unused files

--- test.py
+++
@@ -1,9 +0,0 @@
-
-
-print('|'+'-'*12+'|'+'-'*12+'|'+'-'*23+'|')
-print(f'|\t     |  Computer  |   Spieler/in: {name:>6}  |')
-print('|'+'-'*12+'|'+'-'*12+'|'+'-'*23+'|')
-print(f'|\tWins:| {computer_wins:>8}\t  | {player_wins:>18}\t  |')
-print('|'+'-'*12+'|'+'-'*12+'|'+'-'*23+'|')
-print(f'|     Losses:| {computer_wins:>8}\t  | {player_wins:>18}\t  |')
-print('|'+'-'*12+'|'+'-'*12+'|'+'-'*23+'|')
