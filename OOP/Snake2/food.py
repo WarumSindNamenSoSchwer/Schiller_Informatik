836502020ead9d1aa808f943090a9510a62c10bd
WarumSindNamenSoSchwer <murat.meric@proton.me>
2024-07-01 22:50:08 +0200

    look of the apple changed to circle from square

--- OOP/Snake2/food.py
+++ OOP/Snake2/food.py
@@ -5,7 +5,7 @@ class Food(Turtle):
     def __init__(self):
         super().__init__()
         self.speed(0)
-        self.shape("square")
+        self.shape("circle")
         self.color("red")
         self.penup()
         self.new_location()
